11022d4eb02e39daf227a22b7c9332b232040a44
Simi O <coolkid1984@ymail.com>
2023-11-10 15:52:23 -0600

    second test good pass for years passed

--- src/js/yearsPassed.js
+++ src/js/yearsPassed.js
@@ -18,6 +18,7 @@ export default class AgeCalculator {
     return {
       earth: yearsPassedEarth,
       mercury: this.calculateAgeOnPlanet('mercury') - (age / this.planetaryRatios['mercury']),
+      venus: this.calculateAgeOnPlanet('venus') - (age / this.planetaryRatios['venus']),
     }
   }
 }
