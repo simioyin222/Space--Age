a52e81aa021c1c67e80990de502651fd761dd280
Simi O <coolkid1984@ymail.com>
2023-11-10 17:18:44 -0600

    fix test issue and good pass for earth years passed

--- __test__/yearsPassed.test.js
+++ __test__/yearsPassed.test.js
@@ -1,23 +1,22 @@
-import AgeCalculator from "../src/js/yearsPassed.js";
+import AgeCalculator from "../src/js/yearsPassed.js"
 
 describe("AgeCalculator", () => {
   let calculator
 
   beforeEach(() => {
     calculator = new AgeCalculator(56)
-  });
+  })
   test("should correctly calculate years passed since age 43 on all planets", () => {
-    const yearsPassed = calculator.yearsSince(43);
-    expect(yearsPassed.earth).toEqual(13);
-    expect(yearsPassed.mercury).toEqual(54.16666666666666);
-    expect(yearsPassed.venus).toEqual(20.967741935483872);
-    expect(yearsPassed.mars).toEqual(6.914893617021278);
-    expect(yearsPassed.jupiter).toEqual(1.0961214165261386);
-  });
+    const yearsPassed = calculator.yearsSince(43)
+    expect(yearsPassed.earth).toEqual(13)
+    expect(yearsPassed.mercury).toEqual(54.16666666666666)
+    expect(yearsPassed.venus).toEqual(20.967741935483872)
+    expect(yearsPassed.mars).toEqual(6.914893617021278)
+    expect(yearsPassed.jupiter).toEqual(1.0961214165261386)
+  })
 
   test("should correctly calculate years until age 61 on all planets", () => {
-    const yearsUntil = calulator.yearsUntil(61);
-  }
-
-  )
-});
+    const yearsUntil = calulator.yearsUntil(61)
+    expect(yearsUntil.earth).toEqual(5);
+  })
+})
--- src/js/yearsPassed.js
+++ src/js/yearsPassed.js
@@ -1,33 +1,38 @@
 export default class AgeCalculator {
   constructor(age) {
-    this.age = age;
+    this.age = age
     this.planetaryRatios = {
       mercury: 0.24,
       venus: 0.62,
       mars: 1.88,
-      jupiter: 11.86
-    };
+      jupiter: 11.86,
+    }
   }
 
   calculateAgeOnPlanet(planet) {
-    return this.age / this.planetaryRatios[planet];
+    return this.age / this.planetaryRatios[planet]
   }
 
   yearsSince(age) {
-    const yearsPassedEarth = this.age - age;
+    const yearsPassedEarth = this.age - age
     return {
       earth: yearsPassedEarth,
-      mercury: this.calculateAgeOnPlanet("mercury") - (age / this.planetaryRatios["mercury"]),
-      venus: this.calculateAgeOnPlanet("venus") - (age / this.planetaryRatios["venus"]),
-      mars: this.calculateAgeOnPlanet("mars") - (age / this.planetaryRatios["mars"]),
-      jupiter: this.calculateAgeOnPlanet("jupiter") - (age / this.planetaryRatios["jupiter"])
-    };
+      mercury:
+        this.calculateAgeOnPlanet("mercury") -
+        age / this.planetaryRatios["mercury"],
+      venus:
+        this.calculateAgeOnPlanet("venus") -
+        age / this.planetaryRatios["venus"],
+      mars:
+        this.calculateAgeOnPlanet("mars") - age / this.planetaryRatios["mars"],
+      jupiter:
+        this.calculateAgeOnPlanet("jupiter") -
+        age / this.planetaryRatios["jupiter"],
+    }
   }
 
   yearsUntil(age) {
-    const yearsToEarth = age - this.age;
-    return {
-      earth: yearsToEarth,
-    };
+    const yearsToEarth = age - this.age
+    return {}
   }
 }
