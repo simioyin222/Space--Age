0cf7ca5f8f1f70c97f1c290d8924bad5df3a8d16
Simi O <coolkid1984@ymail.com>
2023-11-10 14:59:40 -0600

    add first test for years pass good fail

--- __test__/yearsPassed.test.js
+++ __test__/yearsPassed.test.js
@@ -6,5 +6,8 @@ describe("AgeCalculator", () => {
   beforeEach(() => {
     calculator = new AgeCalculator(56)
   });
-  
+  test("should correctly calculate years passed since age 43 on all planets", () => {
+    const yearsPassed = calculator.yearsSince(43);
+    expect(yearsPassed.earth).toEqual(13);
+  });
 });
