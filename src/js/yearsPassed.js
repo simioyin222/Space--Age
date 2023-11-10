dbcb66d96520be0c3d50cfd94fc9f925bcfaf6b7
Simi O <coolkid1984@ymail.com>
2023-11-10 15:13:30 -0600

    good pass

--- src/js/yearsPassed.js
+++ src/js/yearsPassed.js
@@ -1,11 +1,22 @@
 export default class AgeCalculator {
   constructor(age) {
-    this.age = age
+    this.age = age;
     this.planetaryRatios = {
       mercury: 0.24,
       venus: 0.62,
       mars: 1.88,
-      jupiter: 11.86,
+      jupiter: 11.86
     };
+  }
+
+  calculateAgeOnPlanet(planet) {
+    return this.age / this.planetaryRatios[planet];
+  }
+
+  yearsSince(age) {
+    const yearsPassedEarth = this.age - age;
+    return {
+      earth: yearsPassedEarth,
+    }
   }
 }
