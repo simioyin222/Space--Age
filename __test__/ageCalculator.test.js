be3f6a27a3df701d6bb98fd9b80ed80fa5c15988
Simi O <coolkid1984@ymail.com>
2023-11-10 13:14:25 -0600

    add new test for years passed 43 from age 65 base also refactor all test with

--- __test__/ageCalculator.test.js
+++ __test__/ageCalculator.test.js
@@ -1,21 +1,24 @@
-import AgeCalculator from '../src/js/ageCalculator';
+import AgeCalculator from "../src/js/ageCalculator"
 
-describe('AgeCalculator', () => {
-  test('should correctly return age in Mercury years', () => {
+describe("AgeCalculator", () => {
+  test("should correctly return age in Mercury years", () => {
     const ageCalculator = new AgeCalculator(30);
     expect(ageCalculator.mercuryAge()).toEqual(7.2);
   });
-  test('should correctly return age in Mars years', () => {
+  test("should correctly return age in Mars years", () => {
     const ageCalculator = new AgeCalculator(30);
     expect(ageCalculator.marsAge()).toEqual(56.4);
   });
-  test('should correctly return age in Venus years', () => {
+  test("should correctly return age in Venus years", () => {
     const ageCalculator = new AgeCalculator(30);
     expect(ageCalculator.venusAge()).toEqual(18.6);
   });
-  test('should correctly return age in Jupiter years', () => {
+  test("should correctly return age in Jupiter years", () => {
     const ageCalculator = new AgeCalculator(30);
     expect(ageCalculator.jupiterAge()).toEqual(355.8);
   });
 });
 
+test('should correctly calculate years passed since age 43 on all planets', () => {
+  const yearsPassed = calculator.yearsSince(43);
+});
