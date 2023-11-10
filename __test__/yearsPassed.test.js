63f560614f6c7b93032fbd0f8c7dbc91d2525255
Simi O <coolkid1984@ymail.com>
2023-11-10 16:22:17 -0600

    add remaning years passed test

--- __test__/yearsPassed.test.js
+++ __test__/yearsPassed.test.js
@@ -10,6 +10,8 @@ describe("AgeCalculator", () => {
     const yearsPassed = calculator.yearsSince(43);
     expect(yearsPassed.earth).toEqual(13);
     expect(yearsPassed.mercury).toEqual(54.16666666666666);
-    expect(yearsPassed.venus).toEqual(20.96);
+    expect(yearsPassed.venus).toEqual(20.967741935483872);
+    expect(yearsPassed.mars).toEqual(6.91);
+    expect(yearsPassed.jupiter).toEqual(1.09);
   });
 });
