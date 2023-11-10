6f81da91a65364c8bc7c561b1f0ed39c0f37a5f8
Simi O <coolkid1984@ymail.com>
2023-11-10 15:33:28 -0600

    second test good fail for years passed

--- __test__/yearsPassed.test.js
+++ __test__/yearsPassed.test.js
@@ -10,5 +10,6 @@ describe("AgeCalculator", () => {
     const yearsPassed = calculator.yearsSince(43);
     expect(yearsPassed.earth).toEqual(13);
     expect(yearsPassed.mercury).toEqual(54.16666666666666);
+    expect(yearsPassed.venus).toEqual(20.96);
   });
 });
